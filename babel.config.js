8ad517c34c7182c19510093f442a4bcf3da20602
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-23 22:48:08 +0530

    update nativewind and tailwindcss

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,6 @@ module.exports = function (api) {
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins,
+    plugins: ["native-wind/babel"]
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}"
+ ],
 
-  presets: [require('nativewind/preset')],
+  // presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
