4aa8805a31f9184b188ab74c981cd8cb7ccf90b4
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-23 22:35:32 +0530

    add Tailwind CSS and Firebase configuration; implement Signin screen layout

--- app/index.tsx
+++ app/index.tsx
@@ -3,25 +3,3 @@ import { Redirect } from "expo-router";
 export default function Page() {
 	return <Redirect href="/signin" />;
 }
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		alignItems: "center",
-// 		padding: 24,
-// 	},
-// 	main: {
-// 		flex: 1,
-// 		justifyContent: "center",
-// 		maxWidth: 960,
-// 		marginHorizontal: "auto",
-// 	},
-// 	title: {
-// 		fontSize: 64,
-// 		fontWeight: "bold",
-// 	},
-// 	subtitle: {
-// 		fontSize: 36,
-// 		color: "#38434D",
-// 	},
-// });
